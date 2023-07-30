5ba8385a117196ae76645955a4ed18f84cb1abe6
alhabsi16 <mxmx8283@gmail.com>
2023-07-30 10:07:21 +0400

    Dictionary

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Dictionary
+{
+    internal class Person
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string Gander { get; set; }
+
+        public Person(long Id, string Name, string Gander) 
+        {
+            this.Id = Id;
+            this.Name = Name;
+            this.Gander = Gander;
+        }
+        public override string ToString()
+        {
+            return ($"name: {Name}, Gander: {Gander}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace C_Sharp_Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person(1102, "Osama", "Male");
+            Person person2 = new Person(1155, "Salim", "Male");
+            Person person3 = new Person(1225, "Alanood", "Female");
+
+            Dictionary<long, Person> personMap = new Dictionary<long, Person>();
+            //personMap.Add(person1.Id, person1);
+            //personMap.Add(person2.Id, person2);
+            //personMap.Add(person3.Id, person3);
+            
+            personMap[person1.Id] = person1;
+            personMap[person2.Id] = person2;
+            personMap[person3.Id] = person3;
+
+            person1.Gander = "Female";
+            personMap[person1.Id] = person2;
+            //personMap.TryAdd(person1.Id, person1);
+            Person? result = null;
+           
+            if(personMap.TryGetValue(1155, out result))
+            {
+                Console.WriteLine(result);
+            }
+
+        }
+    }
+}
